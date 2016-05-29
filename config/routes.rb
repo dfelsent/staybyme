3155e46c75512184b9c60498e717e071103bb614
Deb <dfelsent@gmail.com>
2016-05-29 09:57:37 -0400

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   get 'home/index'
 
   devise_for :users
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
